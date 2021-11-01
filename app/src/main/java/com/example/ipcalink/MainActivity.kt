acbf173f1902a35ce2e074e3ee6a6eeb2172c363
nelson <a20142@alunos.ipca.pt>
2021-11-01 11:47:38 +0000

    1st commit

--- app/src/main/java/com/example/ipcalink/MainActivity.kt
+++ app/src/main/java/com/example/ipcalink/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ipcalink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
