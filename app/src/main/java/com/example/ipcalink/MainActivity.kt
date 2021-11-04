b76aafc0992806dfeef5f0ba95be55c2a5b799df
EduardoDuarte <a20139@alunos.ipca.pt>
2021-11-04 17:13:48 +0000

    login, register, singout v1

--- app/src/main/java/com/example/ipcalink/MainActivity.kt
+++ app/src/main/java/com/example/ipcalink/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.ipcalink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ipcalink.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
 }
