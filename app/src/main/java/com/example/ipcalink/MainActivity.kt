2795328d57805c2ffdf6ab1820caefe831408591
JMFS4 <45976951+JMFS4@users.noreply.github.com>
2021-12-09 22:37:54 +0000

    -A parte de aparecer uma notificação quando o utilizador manda uma mensagem está feita. -Acabei a parte de encriptar e desencriptar as mensagens dos utilizadores. -Consigo ir buscar as notificações guardadas na base de dados, mas ainda não consigo fazer isso em real time, tenho de alterar como as notificações são guardadas na base de dados. -Consigo guardar a chave secreta na base de dados e a ir buscar se o utilizador não a tiver, mas ainda tenho umas coisas a mudar.

--- app/src/main/java/com/example/ipcalink/MainActivity.kt
+++ app/src/main/java/com/example/ipcalink/MainActivity.kt
@@ -1,19 +1,55 @@
 package com.example.ipcalink
 
 import android.content.Intent
-import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.*
+import java.util.*
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.ipcalink.databinding.ActivityMainBinding
-import com.example.ipcalink.login.LoginActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    /*var notificationReceiver : NotificationReceiver? = null
+
+    inner class NotificationReceiver : BroadcastReceiver() {
+        override fun onReceive(p0: Context?, intent: Intent?) {
+            //Get the message head
+            intent?.extras?.getString(MyFirebaseMessagingService.NOTIFICATION_HEAD)?.let { it1 ->
+                intent?.extras?.getString(MyFirebaseMessagingService.NOTIFICATION_BODY)?.let { it2->
+                    //here the message is being configured
+                    alertNotificatio(this@MainActivity, it1, it2)
+                }
+            }
+        }
+    }*/
+
+   /* override fun onResume() {
+        super.onResume()
+        notificationReceiver = NotificationReceiver()
+
+        //activates the Notification Receiver
+        this.registerReceiver(notificationReceiver, IntentFilter(MyFirebaseMessagingService.BROADCAST_NEW_NOTIFICATION))
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        //deactivates the Notification Receiver
+        notificationReceiver?.let {
+            this.unregisterReceiver(it)
+        }
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +57,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.HomeFragment,
+                R.id.PushNotificationFragment,
+                R.id.ShowNotificationFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
-
-    fun signOut(view: View) {
-        Firebase.auth.signOut()
-        startActivity(Intent(this, LoginActivity::class.java ))
-        finish()
-    }
-
-
-
 }
